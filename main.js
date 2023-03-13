d821e3d1af91b048843d9efcbd62358872ec2099
Reggie <reggie1se@gmail.com>
2023-03-13 12:26:12 -0400

    removed repeating color selection

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function noRepeatRandomNumber( curr_num ) {
+  
+}
+
+let last_number;
+
 const colors = [
   "BlueViolet",
   "DarkMagenta",
@@ -25,6 +31,15 @@ let max = 14;
 button.addEventListener("click", () => {
   let colorSelection = randomNumber(0, max);
   console.log( colorSelection );
+  if( last_number ) {
+    while( colorSelection == last_number ) {
+        console.log( `last no. = ${last_number}` );
+        colorSelection = randomNumber(0, max);
+        console.log( `colorSelection = ${colorSelection}` );
+    }
+  }
+  
+  last_number = colorSelection;
   document.body.style.background = colors[colorSelection];
 });
 
